8a1c713ee35f161538848b057d10ccf9aaec061f
skerjie <skerjie@live.com>
2016-09-01 22:52:34 +0300

    Доделал крестик, сделал анимацию

--- MyRestraunts/QuickReviewViewController.swift
+++ MyRestraunts/QuickReviewViewController.swift
@@ -11,14 +11,20 @@ import UIKit
 class QuickReviewViewController: UIViewController {
   
   @IBOutlet weak var backgrounImageView: UIImageView!
+  @IBOutlet weak var windowView : UIView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    // создание brur эффекта
     var darkBlurEffect = UIBlurEffect(style: UIBlurEffectStyle.dark)
     var darkBlurEffectView = UIVisualEffectView(effect: darkBlurEffect)
     darkBlurEffectView.frame = view.bounds
     backgrounImageView.addSubview(darkBlurEffectView)
+    
+    // создание анимации для вьюшки с кнопками
+    
+    windowView.transform = CGAffineTransform(scaleX: 0.0, y: 0.0)
+    
   }
   
   override func didReceiveMemoryWarning() {
@@ -26,6 +32,13 @@ class QuickReviewViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
+  //  анимация для вьюшки с кнопками
+  override func viewDidAppear(_ animated: Bool) {
+    UIView.animate(withDuration: 0.6, delay: 0.0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.beginFromCurrentState, animations: {
+      self.windowView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+
+      }, completion: nil)
+  }
   
   /*
    // MARK: - Navigation
